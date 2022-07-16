9dd97a6598688f4f6f886bf4e1d30b77e6d7a01e
PooriaT <pooria.heyboy@gmail.com>
2022-07-16 15:15:00 -0400

    creating recipe DB

--- cookingRobot/recipeManager/models.py
+++ cookingRobot/recipeManager/models.py
@@ -1,3 +1,27 @@
+from enum import unique
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+# Here we create tables for our DB.
+
+#UserProfile Class -> referring to user information 
+class UserProfile(models.Model):
+    pass
+
+#Recipe Class -> Referring to recipe information
+class Recipe(models.Model):
+    recipeID = models.AutoField(primary_key=True)
+    recipeName = models.TextField(max_length=50)
+    ingredient = models.TextField(max_length=200)
+    duration = models.DurationField()
+    steps = models.TextField(max_length=2000)
+    calories = models.FloatField()
+    servings = models.FloatField()
+    chefName = models.TextField(max_length=50, default='ADMIN')
+    
+    
+
+#List Class -> Referring to list information
+class List(models.Model):
+    pass
+
--- cookingRobot/recipeManager/views.py
+++ cookingRobot/recipeManager/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+from django.http import HttpResponse
+
+from .models import UserProfile
+from .models import Recipe
+from .models import List
+
+def index(request):
+    
+    obj = Recipe.objects.all()
+    #output="test"
+    template = loader.get_template('recipeManager/index.html')
+    context = {
+        'obj': obj,
+    }
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse(output)

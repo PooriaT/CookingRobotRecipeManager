66c48bf90b97a474aa728c75513694a84950c470
Arminjnezhad <minookj@gmail.com>
2022-07-28 20:58:21 -0400

    Model Changes

--- cookingRobot/recipeManager/models.py
+++ cookingRobot/recipeManager/models.py
@@ -4,20 +4,40 @@ from django.db import models
 
 # Here we create tables for our DB.
 
-#UserProfile Class -> referring to user information 
-class UserProfile(models.Model):
-    pass
+
+#Ingredients Category
+class IngCategory(models.Model):
+    categoryID = models.AutoField(primary_key=True)
+    categoryName = models.CharField(max_length=50)
+
+#Ingredients
+class Ingredients(models.Model):
+    ingID = models.AutoField(primary_key=True)
+    ingName = models.CharField(max_length=50)
+    ingCalPerHundered = models.FloatField()
+    ingCategory = models.ForeignKey('IngCategory', on_delete=models.CASCADE, blank=True) #one to many
+
+#Recipe Category
+class RecipeCategory(models.Model):
+    categoryID = models.AutoField(primary_key=True)
+    categoryName = models.CharField(max_length=50)
+
+#Utensils
+class Utensils(models.Model):
+    utensilID = models.AutoField(primary_key=True)
+    utensilName = models.CharField(max_length=50)
 
 #Recipe Class -> Referring to recipe information
 class Recipe(models.Model):
     recipeID = models.AutoField(primary_key=True)
-    recipeName = models.TextField(max_length=50)
-    ingredient = models.TextField(max_length=200)
+    recipeName = models.CharField(max_length=50)
+    category = models.ManyToManyField(RecipeCategory) #many to many
+    ingredient = models.ManyToManyField(Ingredients) #many to many
     duration = models.DurationField()
-    steps = models.TextField(max_length=2000)
-    calories = models.FloatField()
+    steps = models.CharField(max_length=2000)
     servings = models.FloatField()
     chefName = models.TextField(max_length=50, default='ADMIN')
+    recipeUtensil = models.ForeignKey('Utensils', on_delete=models.CASCADE, blank=True)
     
     
 

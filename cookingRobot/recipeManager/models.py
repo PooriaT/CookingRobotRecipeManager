21bb186be21ff7b82ff46809e0ada5b26c910c83
PooriaT <pooria.heyboy@gmail.com>
2022-08-03 20:57:18 -0400

    ingredient

--- cookingRobot/recipeManager/models.py
+++ cookingRobot/recipeManager/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
@@ -11,26 +12,26 @@ User._meta.get_field('email')._unique = True
 #Ingredient Category Class
 class IngredientCategory(models.Model):
     ingredientCategoryID = models.AutoField(primary_key=True)
-    ingredientCategoryName = models.CharField(max_length=50)
+    ingredientCategoryName = models.CharField(max_length=50, unique=True)
 
 #Ingredient class
 class Ingredient(models.Model):
     ingredientID = models.AutoField(primary_key=True)
-    ingredientName = models.CharField(max_length=50)
+    ingredientName = models.CharField(max_length=50, unique=True)
     baseCalorie = models.FloatField()
     ingredientCategory = models.ForeignKey('IngredientCategory', on_delete=models.CASCADE)
-    ingredientImg = models.ImageField(upload_to ='ingredient/', blank=True, null=True)
+    ingredientImg = models.ImageField(upload_to ='ingredient/', blank=True, null=True, default='default/noImage.jpeg')
 
 #Utensils Class
 class Utensil(models.Model):
     utensilID = models.AutoField(primary_key=True)
-    utensilName = models.CharField(max_length=50)
-    utensilImg = models.ImageField(upload_to ='utensil', blank=True, null=True) 
+    utensilName = models.CharField(max_length=50, unique=True)
+    utensilImg = models.ImageField(upload_to ='utensil', blank=True, null=True, default='default/noImage.jpeg') 
     
 #Category class
 class RecipeCategory(models.Model):
     categoryID = models.AutoField(primary_key=True)
-    categoryName = models.CharField(max_length=50) 
+    categoryName = models.CharField(max_length=50, unique=True) 
 
 #Recipe Class -> Referring to recipe information
 class Recipe(models.Model):
@@ -44,8 +45,9 @@ class Recipe(models.Model):
     steps = models.TextField(max_length=3000)
     servings = models.FloatField()
     calories = models.FloatField()
-    rating = models.IntegerField(default=0)
-    recipeImg = models.ImageField(upload_to ='recipe/', blank=True, null=True)
+    rating = models.FloatField(default=0)
+    numRater = models.IntegerField(default=0)
+    recipeImg = models.ImageField(upload_to ='recipe/', blank=True, null=True, default='default/noImage.jpeg')
     chefName = models.CharField(max_length=50, default='admin')
     
     
